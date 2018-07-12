9ca0482a706782bf0124cce76c8f48333400baf9
Craig Sussen <Susman777@yahoo.com>
2018-07-12 08:27:26 -0400

    Add files via upload

---
+++ RequestToService.js
@@ -0,0 +1,42 @@
+POST https://restful-api-7778.herokuapp.com/
+
+Content-type: application/json
+
+POST body:
+{
+  "user_1": {
+    "name": "John",
+    "checking": {
+      "balance": "$37,043.31"
+    }
+  },
+
+
+  "user_2": {
+    "name": "Mary",
+    "checking": {
+      "balance": "$11,543.45"
+    }
+  },
+
+  "user_3": {
+    "name": "Susan",
+    "checking": {
+      "balance": "$72,436,851.84"
+    }
+  },
+
+  "user_4": {
+    "name": "Josh",
+    "checking": {
+      "balance": "$0.01"
+    }
+  },
+
+  "user_5": {
+    "name": "Michael",
+    "checking": {
+      "balance": "$80,432.45"
+    }
+  }
+}
