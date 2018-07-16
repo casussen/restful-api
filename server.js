4716543a86e0c162ec0bcf877f55c851b9132414
Craig Sussen <Susman777@yahoo.com>
2018-07-16 18:27:27 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -3,39 +3,7 @@ var express = require('express');
 var app = express();
 
 app.use('/', function(req,res){
-	res.send({
-	"John": {
-		"checking": {
-			"balance": "$37,043.31"
-		}
-	},
-
-
-	"Mary": {
-		"checking": {
-			"balance": "$11,543.45"
-		}
-	},
-
-	"Susan": {
-		"name": "Susan",
-		"checking": {
-			"balance": "$72,436,851.84"
-		}
-	},
-
-	"Josh": {
-		"checking": {
-			"balance": "$0.01"
-		}
-	},
-
-	"Michael": {
-		"checking": {
-			"balance": "$80,432.45"
-		}
-	}
-});
+	res.send('Welcome to my API!');
 });
 
 app.listen(8000, function(){
