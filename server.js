64ac38e5705eaa75147bb77f7b76b7fca55ee7fd
Craig Sussen <Susman777@yahoo.com>
2018-07-17 09:01:51 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -3,7 +3,43 @@ var express = require('express');
 var app = express();
 
 app.use('/', function(req,res){
-	res.send('Welcome to my API!');
+	res.send({
+"user_1": {
+		"name": "John",
+		"checking": {
+			"balance": "$37,043.31"
+		}
+	},
+
+
+	"user_2": {
+		"name": "Mary",
+		"checking": {
+			"balance": "$11,543.45"
+		}
+	},
+
+	"user_3": {
+		"name": "Susan",
+		"checking": {
+			"balance": "$72,436,851.84"
+		}
+	},
+
+	"user_4": {
+		"name": "Josh",
+		"checking": {
+			"balance": "$0.01"
+		}
+	},
+
+	"user_5": {
+		"name": "Michael",
+		"checking": {
+			"balance": "$80,432.45"
+		}
+	}
+});
 });
 
 app.set( 'port', ( process.env.PORT || 5000 ));
